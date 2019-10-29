8b4840b04e6e89f4ece6710fc452cbf424e4d7b7
huangzhaolong <huangzhaolong1234@163.com>
2019-10-29 21:18:40 +0800

    array,88,215

---
+++ array/kth_largest_element_in_an_array_215.py
@@ -0,0 +1,31 @@
+from typing import List
+
+'''
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+'''
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        for i in range(k):
+            for j in range(len(nums) - 1 - i):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        print(nums)
+        return nums[-k]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
---
+++ array/merge_sorted_array_88.py
@@ -0,0 +1,71 @@
+from typing import List
+
+'''
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+'''
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums2:
+            return
+        idx1, idx2 = 0, 0
+        stack = []
+        while idx1 < m + n:
+            if idx1 < m:
+                if idx2 < n:
+                    if nums1[idx1] <= nums2[idx2]:
+                        if stack and stack[0] < nums1[idx1]:
+                            stack.append(nums1[idx1])
+                            nums1[idx1] = stack[0]
+                            stack.pop(0)
+                    if nums2[idx2] < nums1[idx1]:
+                        if (stack and nums2[idx2] <= stack[0]) or (not stack):
+                            stack.append(nums1[idx1])
+                            nums1[idx1] = nums2[idx2]
+                            idx2 += 1
+                        elif stack and stack[0] < nums2[idx2]:
+                            stack.append(nums1[idx1])
+                            nums1[idx1] = stack[0]
+                            stack.pop(0)
+                else:
+                    if stack:
+                        stack.append(nums1[idx1])
+                        nums1[idx1] = stack[0]
+                        stack.pop(0)
+            else:
+                if idx2 < n:
+                    if (stack and nums2[idx2] <= stack[0]) or (not stack):
+                        nums1[idx1] = nums2[idx2]
+                        idx2 += 1
+                    elif stack and stack[0] < nums2[idx2]:
+                        nums1[idx1] = stack[0]
+                        stack.pop(0)
+                else:
+                    nums1[idx1] = stack[0]
+                    stack.pop(0)
+            idx1 += 1
+            print(nums1)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums1 = [1, 2, 4, 5, 6, 0]
+    nums2 = [3]
+    s.merge(nums1, 5, nums2, 1)
+    print(nums1)
